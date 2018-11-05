8d4400b20bdadfec46caa3aacbf79a0a0c255c53
Michael Kelley <michael@minthe.net>
2018-11-05 00:38:16 -0600

    Create activities for the month view and event creation.
    Everything is very rough, but should allow us to implement some
    domain functionality.

--- app/src/main/java/net/minthe/calendarapp/CreateEventActivity.java
+++ app/src/main/java/net/minthe/calendarapp/CreateEventActivity.java
@@ -1,16 +1,19 @@
 package net.minthe.calendarapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MonthViewActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_create_event);
+    }
+
+    public void handleCancel(View view) {
+
+
     }
 }
---
+++ app/src/main/java/net/minthe/calendarapp/MonthViewActivity.java
@@ -0,0 +1,31 @@
+package net.minthe.calendarapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+
+public class MonthViewActivity extends AppCompatActivity  {
+
+    private Button addEventButton;
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_month_view);
+        addEventButton = findViewById(R.id.addEventButton);
+        calendarView = findViewById(R.id.calendarView);
+    }
+
+    public void createEvent(View view) {
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        long date = calendarView.getDate();
+        intent.putExtra("NET_MINTHE_CALENDARAPP_SELECTED_DATE", date);
+        startActivity(intent);
+    }
+}
