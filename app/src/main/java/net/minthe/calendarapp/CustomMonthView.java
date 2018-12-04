dc9ea35478afa10c3e611211b3adc48df5282cd7
Michael Kelley <michael@minthe.net>
2018-12-04 16:43:16 -0600

    Add event list to month view.

--- app/src/main/java/net/minthe/calendarapp/CustomMonthView.java
+++ app/src/main/java/net/minthe/calendarapp/CustomMonthView.java
@@ -2,6 +2,9 @@ package net.minthe.calendarapp;
 
 import android.os.Bundle;
 
+import net.minthe.calendarapp.domain.Event;
+import net.minthe.calendarapp.domain.MonthDetails;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -9,7 +12,7 @@ import java.util.GregorianCalendar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
-public class CustomMonthView extends AppCompatActivity {
+public class CustomMonthView extends AppCompatActivity implements EventFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,21 @@ public class CustomMonthView extends AppCompatActivity {
         Calendar c = GregorianCalendar.getInstance();
         c.setTime(new Date());
         long date = c.getTimeInMillis();
-        MonthFragment f = MonthFragment.newInstance(date);
+
+        MonthDetails md = new MonthDetails(date);
+
+        MonthFragment monthFragment = MonthFragment.newInstance(date);
+        EventFragment eventFragment = EventFragment.newInstance(1, md.getMonthStart(), md.getMonthEnd());
+
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction()
-                .replace(R.id.cmvFrame, f)
+                .replace(R.id.cmvFrame, eventFragment, "evFrag")
+                .replace(R.id.cmvFrame, monthFragment, "cmFrag")
                 .commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(Event item) {
+
     }
 }
