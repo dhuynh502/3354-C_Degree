6fc044fbfa2829f24a94aa104eb6a403feac6112
Michael Kelley <michael@minthe.net>
2018-12-05 15:56:43 -0600

    Fix test for MonthDetails#getMonth

--- app/src/test/java/net/minthe/calendarapp/MonthDetailsTest.java
+++ app/src/test/java/net/minthe/calendarapp/MonthDetailsTest.java
@@ -52,18 +52,33 @@ public class MonthDetailsTest {
     public void testPrevMonth() {
 
     }
+
     @Test
     public void testMonth(){
-        final int[] years = {2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007};
-        //testing on MonthDetails.getMonth() function
-        Calendar c = GregorianCalendar.getInstance();
-        for (int i = 0; i < years.length; i++) {
-            c.clear();
-            c.set(Calendar.YEAR, years[i]);
-            MonthDetails md = new MonthDetails(c.getTimeInMillis());
-            assertEquals(years[i], md.getMonth());
+        // These timestamps represent 15 {Month} 2018, 00:00:00
+        final long[] instants = {
+                1515974400000L, // Jan
+                1518652800000L, // Feb
+                1521072000000L, // Mar
+                1523750400000L, // April
+                1526342400000L, // May
+                1529020800000L, // June
+                1531612800000L, // July
+                1534291200000L, // August
+                1536969600000L, // September
+                1539561600000L, // October
+                1542240000000L, // November
+                1544832000000L  // December
+        };
+
+        for (int i = 0; i < instants.length; i++) {
+            MonthDetails md = new MonthDetails(instants[i]);
+
+            // The index of each month matches the ordering
+            assertEquals(i, md.getMonth());
         }
     }
+
     @Test
     public void testYear() {
         final int[] years = {2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007};
