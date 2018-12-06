d221a9d64a5106934b0b60267eec9dfeb1e015f0
kitersi <42045538+kitersi@users.noreply.github.com>
2018-12-06 02:02:38 -0600

    Created Test Class
    Test Class to test getDurationSeconds() method in CreateEventActivity

---
+++ app/src/test/java/net/minthe/calendarapp/CreateEventTest.java
@@ -0,0 +1,51 @@
+package net.minthe.calendarapp;
+
+import android.widget.EditText;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+
+import static org.junit.Assert.*;
+
+public class CreateEventTest {
+
+    CreateEventActivity createEventTest;
+
+    // Constructor
+    public CreateEventTest() {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        createEventTest = new CreateEventActivity();
+    }
+
+    @Test
+    public void getDurationSeconds() throws Exception{
+        Clock clock = Clock.systemDefaultZone();
+        ZoneId zoneId = ZoneId.systemDefault();
+
+        // Set starting time in past
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
+        // Convert LocalDateTime format to Date format
+        Date start = new Date();
+        oneWeekAgo = LocalDateTime.ofInstant(start.toInstant(), ZoneId.systemDefault());
+        Date startDate = Date.from(oneWeekAgo.atZone(ZoneId.systemDefault()).toInstant());
+
+        // Set ending time to current time
+        LocalDateTime current = LocalDateTime.now();
+        // Convert LocalDateTime format to Date format
+        Date end = new Date();
+        current = LocalDateTime.ofInstant(start.toInstant(), ZoneId.systemDefault());
+        Date endDate = Date.from(current.atZone(ZoneId.systemDefault()).toInstant());
+
+        assertEquals(-1, createEventTest.getDurationSeconds(startDate, endDate));
+    }
+}
