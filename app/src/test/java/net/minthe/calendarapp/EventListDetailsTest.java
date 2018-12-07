d1256053caf88a936b2ed9ec744f7bbb287d156f
dhuynh502 <dhuynh502@gmail.com>
2018-12-07 15:27:14 -0600

    More @Test

---
+++ app/src/test/java/net/minthe/calendarapp/EventListDetailsTest.java
@@ -0,0 +1,38 @@
+package net.minthe.calendarapp;
+
+import org.junit.Test;
+import net.minthe.calendarapp.domain.EventListDetails;
+import net.minthe.calendarapp.domain.MonthDetails;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
+public class EventListDetailsTest {
+
+    @Test
+    public void getEventsForDay() {
+        Calendar c = GregorianCalendar.getInstance();
+        int start =0;
+        int end = 0;
+        for (int i =0; i<31;i++)
+        {
+            end = start +24 * 60*60*1000;
+
+        }
+        assertEquals(end, start);
+    }
+    @Test
+    public void testYear() {
+        final int[] years = {2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007};
+        //testing on MonthDetails.getYear() function
+        Calendar c = GregorianCalendar.getInstance();
+        for (int i = 0; i < years.length; i++) {
+            c.clear();
+            c.set(Calendar.YEAR, years[i]);
+            MonthDetails md = new MonthDetails(c.getTimeInMillis());
+            assertEquals(years[i], md.getYear());
+        }
+    }
+}
