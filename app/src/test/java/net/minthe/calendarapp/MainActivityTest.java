3ec188b7194545fcb8d25bc665ee8fe4073bee14
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2018-12-07 23:55:02 -0600

    Functional Test case for Activity Launch, specifically customMonthView

--- app/src/test/java/net/minthe/calendarapp/MainActivityTest.java
+++ app/src/test/java/net/minthe/calendarapp/MainActivityTest.java
@@ -2,15 +2,33 @@ package net.minthe.calendarapp;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+/*
+  Class test the buttons for functionanility
+  so far we are testing the CustomMonthView Button
+ */
 public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    private MainActivity mActivity = null;
+
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(CustomMonthView.class.getName(),null,false);
 
     @Before
     public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
     }
+    @Test
+    public void testLaunchOfCustomMonthView(){
+        assertNotNull(mActivity.findViewById(R.id.customMonthButton));
+        onView(withId.(R.id.customMonthButton)).perform(click());
 
+        Activity customMonthView = getInstrumentation().waitForMonitorWithTimeout(monitor,4000);
+        asserNotNull(customMonthView);
+    }
     @After
     public void tearDown() throws Exception {
     }
